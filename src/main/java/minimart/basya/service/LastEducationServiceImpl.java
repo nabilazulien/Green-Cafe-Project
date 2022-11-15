4242c48575652a6343e2d14ea4bed9e347db4aa8
Nabila <nabilabasya.business@gmail.com>
2022-11-15 16:51:48 +0700

    feat save job position and las education

--- src/main/java/minimart/basya/service/LastEducationServiceImpl.java
+++ src/main/java/minimart/basya/service/LastEducationServiceImpl.java
@@ -14,6 +14,7 @@ public class LastEducationServiceImpl implements LastEducationService {
     private LastEducationRepository lastEducationRepository;
     @Override
     public List getNewLastEducation() {
+
         return lastEducationRepository.findAll();
     }
 }
--- src/main/java/minimart/basya/service/PaymentTypeService.java
+++ src/main/java/minimart/basya/service/PaymentTypeService.java
@@ -4,5 +4,5 @@ import java.util.List;
 
 public interface PaymentTypeService {
     List getNewPaymentType ();
-    
+
 }
